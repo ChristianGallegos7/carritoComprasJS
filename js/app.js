38f4ccbfbc4215d264ac3d099865d440a2753ef9
Christian <christiantenz7@gmail.com>
2023-06-14 16:17:32 -0500

    Finalizado

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 //Variables
 const carrito = document.getElementById("carrito");
 const cursos = document.getElementById("lista-cursos");
-const listaCursos = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+const listaCursos = document.querySelector("#lista-carrito tbody");
+const vaciarCarritoBtn = document.getElementById("vaciar-carrito");
 
 //Event Listeners
 cargarEventListeners();
@@ -11,9 +11,11 @@ function cargarEventListeners() {
   //Dispara cuando se presiona agregar-carrito
   cursos.addEventListener("click", comprarCurso);
   //se elimina un curso del carrito
-  carrito.addEventListener('click', elimiarCurso);
+  carrito.addEventListener("click", elimiarCurso);
   //eliminar todos los cursos
-  vaciarCarritoBtn.addEventListener('click',vaciarCarrito)
+  vaciarCarritoBtn.addEventListener("click", vaciarCarrito);
+
+  document.addEventListener("DOMContentLoaded", leerLocalStorage);
 }
 
 //Funciones
@@ -22,7 +24,7 @@ function cargarEventListeners() {
 function comprarCurso(e) {
   e.preventDefault();
 
-  if(e.target.classList.contains('agregar-carrito')){
+  if (e.target.classList.contains("agregar-carrito")) {
     let curso = e.target.parentElement.parentElement;
     leerDatosCurso(curso);
   }
@@ -31,16 +33,16 @@ function comprarCurso(e) {
 //Lee los datos del curso
 function leerDatosCurso(curso) {
   const infoCurso = {
-    imagen: curso.querySelector('img').src,
-    titulo: curso.querySelector('h4').textContent,
-    precio: curso.querySelector('.precio span').textContent,
-    id: curso.querySelector('a').getAttribute('data-id')
-  }
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+  };
   insertarCarrito(infoCurso);
 }
 
-function insertarCarrito(curso){
-  const row = document.createElement('tr');
+function insertarCarrito(curso) {
+  const row = document.createElement("tr");
   row.innerHTML = `
       <td>
         <img src="${curso.imagen}" width="100">
@@ -56,27 +58,72 @@ function insertarCarrito(curso){
       </td>
   `;
   listaCursos.appendChild(row);
-
+  guardarCursoLocalStorage(curso);
 }
 //Elimina el curso del carrito en el dom
-function elimiarCurso(e){
+function elimiarCurso(e) {
   e.preventDefault();
   let curso;
-  if(e.target.classList.contains('borrar-curso')){
+  if (e.target.classList.contains("borrar-curso")) {
     e.target.parentElement.parentElement.remove();
   }
 }
 
 //vaciar el carrito con un solo boton
 
-function vaciarCarrito(){
-
+function vaciarCarrito() {
   //forma corta pero lentav
   // listaCursos.innerHTML = "";
   //forma recomendada
-  while(listaCursos.firstChild){
+  // alert("Seguro quiere vaciar el carrito?");
+  while (listaCursos.firstChild) {
     listaCursos.removeChild(listaCursos.firstChild);
   }
   return false;
 }
 
+//Almacena curso en el local storage
+
+function guardarCursoLocalStorage(curso) {
+  let cursos;
+  cursos = obtenerCursoLocalStorage();
+  cursos.push(curso);
+  localStorage.setItem("cursos", JSON.stringify(cursos));
+}
+//comprueba que hay elementos en ls
+function obtenerCursoLocalStorage() {
+  let cursosLS;
+
+  //COMPROBAR SI HAY ALGO EN EL LOCAL STORAGE
+  if (localStorage.getItem("cursos") === null) {
+    cursosLS = [];
+  } else {
+    cursosLS = JSON.parse(localStorage.getItem("cursos"));
+  }
+  return cursosLS;
+}
+
+function leerLocalStorage() {
+  let cursosLS;
+
+  cursosLS = obtenerCursoLocalStorage();
+
+  cursosLS.forEach(function(curso){
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>
+        <img src="${curso.imagen}" width="100">
+      </td>
+      <td>
+        ${curso.titulo}
+      </td>
+      <td>
+        ${curso.precio}
+      </td>
+      <td>
+        <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+      </td>
+  `;
+  listaCursos.appendChild(row)
+  });
+}
